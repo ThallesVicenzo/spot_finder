56990b496d68b2f12b6f70415620da6ac46e45fb
Thalles Vicenzo Costa <thalles.vicenzo.costa@gmail.com>
2023-04-11 11:46:51 -0300

    feat: add a new Provider & new size for texts

--- lib/main.dart
+++ lib/main.dart
@@ -1,9 +1,10 @@
 import 'package:flutter/material.dart';
 import 'package:provider/provider.dart';
 import 'package:spot_finder/view-model/providers/authentication_provider.dart';
-import 'package:spot_finder/view-model/providers/camera_provider.dart';
-import 'package:spot_finder/view-model/markers/markers.dart';
+// import 'package:spot_finder/view-model/providers/camera_provider.dart';
+import 'package:spot_finder/view-model/providers/markers_provider.dart';
 
+import 'view-model/providers/custom_text_field_provider.dart';
 import 'view-model/routes/named_routes.dart';
 import 'view-model/routes/routes.dart';
 
@@ -13,7 +14,8 @@ void main() {
       providers: [
         ChangeNotifierProvider(create: (_) => MarkersProvider()),
         ChangeNotifierProvider(create: (_) => AuthenticationProvider()),
-        ChangeNotifierProvider(create: (_) => CameraProvider())
+        // ChangeNotifierProvider(create: (_) => CameraProvider())
+        ChangeNotifierProvider(create: (_) => TextFieldProvider())
       ],
       child: const MapHelper(),
     ),
@@ -31,14 +33,19 @@ class MapHelper extends StatelessWidget {
       theme: ThemeData(
         primaryColor: const Color(0xFF11159A),
         primaryTextTheme: const TextTheme(
-            bodyLarge: TextStyle(
-              color: Color(0xFF11159A),
-              fontSize: 50,
-            ),
-            bodyMedium: TextStyle(
-              color: Color(0xFF11159A),
-              fontSize: 17,
-            )),
+          bodyLarge: TextStyle(
+            color: Color(0xFF11159A),
+            fontSize: 50,
+          ),
+          bodyMedium: TextStyle(
+            color: Color(0xFF11159A),
+            fontSize: 30,
+          ),
+          bodySmall: TextStyle(
+            color: Color(0xFF11159A),
+            fontSize: 17,
+          ),
+        ),
       ),
     );
   }
