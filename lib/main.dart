fc49ca4ffbc27db38a84f3792dc44d075067b6da
Thalles Vicenzo Costa <thalles.vicenzo.costa@gmail.com>
2023-04-24 21:39:25 -0300

    refactor: change imports name & move ThemeData

--- lib/main.dart
+++ lib/main.dart
@@ -1,9 +1,9 @@
 import 'package:flutter/material.dart';
 import 'package:provider/provider.dart';
-import 'package:spot_finder/view-model/providers/authentication_provider.dart';
-// import 'package:spot_finder/view-model/providers/camera_provider.dart';
-import 'package:spot_finder/view-model/providers/markers_provider.dart';
 
+import 'shared/theme.dart';
+import 'view-model/providers/authentication_provider.dart';
+import 'view-model/providers/markers_provider.dart';
 import 'view-model/providers/save_spot_provider.dart';
 import 'view-model/routes/named_routes.dart';
 import 'view-model/routes/routes.dart';
@@ -14,7 +14,6 @@ void main() {
       providers: [
         ChangeNotifierProvider(create: (_) => MarkersProvider()),
         ChangeNotifierProvider(create: (_) => AuthenticationProvider()),
-        // ChangeNotifierProvider(create: (_) => CameraProvider())
         ChangeNotifierProvider(create: (_) => SaveSpotProvider())
       ],
       child: const MapHelper(),
@@ -30,25 +29,7 @@ class MapHelper extends StatelessWidget {
     return MaterialApp(
       initialRoute: NamedRoutes.splash,
       routes: Routes.all(context),
-      theme: ThemeData(
-        splashColor: const Color(0xFFFFFFFF),
-        primaryColor: const Color(0xFF11159A),
-        highlightColor: const Color(0xFFFFBE00),
-        primaryTextTheme: const TextTheme(
-          bodyLarge: TextStyle(
-            color: Color(0xFF11159A),
-            fontSize: 50,
-          ),
-          bodyMedium: TextStyle(
-            color: Color(0xFF11159A),
-            fontSize: 30,
-          ),
-          bodySmall: TextStyle(
-            color: Color(0xFF11159A),
-            fontSize: 17,
-          ),
-        ),
-      ),
+      theme: AppTheme.defaultTheme,
     );
   }
 }
