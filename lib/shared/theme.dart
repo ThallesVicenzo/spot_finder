ee0fdaaaddc8f35996cba0698a9b931d5994583c
Thalles Vicenzo Costa <thalles.vicenzo.costa@gmail.com>
2023-04-24 21:39:48 -0300

    feat: create AppTheme class

---
+++ lib/shared/theme.dart
@@ -0,0 +1,25 @@
+import 'package:flutter/material.dart';
+
+class AppTheme {
+  static get defaultTheme => ThemeData(
+        splashColor: const Color(0xFFFFFFFF),
+        primaryColor: const Color(0xFF11159A),
+        cardColor: const Color(0xFFD8D8D8),
+        secondaryHeaderColor: const Color(0xFF000000),
+        highlightColor: const Color(0xFFFFBE00),
+        primaryTextTheme: const TextTheme(
+          bodyLarge: TextStyle(
+            color: Color(0xFF11159A),
+            fontSize: 50,
+          ),
+          bodyMedium: TextStyle(
+            color: Color(0xFF11159A),
+            fontSize: 30,
+          ),
+          bodySmall: TextStyle(
+            color: Color(0xFF11159A),
+            fontSize: 17,
+          ),
+        ),
+      );
+}
