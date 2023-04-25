1a0d39cd9cf488e85b9f87eefd5c16a5eba84b5a
Thalles Vicenzo Costa <thalles.vicenzo.costa@gmail.com>
2023-04-25 16:01:47 -0300

    style: add focusColor

--- lib/shared/theme.dart
+++ lib/shared/theme.dart
@@ -6,6 +6,7 @@ class AppTheme {
         primaryColor: const Color(0xFF11159A),
         cardColor: const Color(0xFFD8D8D8),
         secondaryHeaderColor: const Color(0xFF000000),
+        focusColor: const Color(0xFFF44336),
         highlightColor: const Color(0xFFFFBE00),
         primaryTextTheme: const TextTheme(
           bodyLarge: TextStyle(
