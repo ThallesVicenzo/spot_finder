b6af63bd8ea76acabe775a837bf71366cc54f977
Thalles Vicenzo Costa <thalles.vicenzo.costa@gmail.com>
2023-04-24 21:43:29 -0300

    feat: add errorText & selectColor functions

--- lib/view-model/providers/save_spot_provider.dart
+++ lib/view-model/providers/save_spot_provider.dart
@@ -2,9 +2,28 @@ import 'package:flutter/material.dart';
 
 class SaveSpotProvider with ChangeNotifier {
   TextEditingController textController = TextEditingController();
+  Color currentColor = const Color(0xFFF40B0B);
+
+  Future<String?> errorText() async {
+    final text = textController.value.text;
+    if (text.isEmpty) {
+      return 'Não pode ser vazio!';
+    }
+    if (text.length <= 2) {
+      return 'Não pode ter menos que 3 caracteres.';
+    }
+    notifyListeners();
+
+    return null;
+  }
 
   void onChanged(String value) {
     value = textController.text;
+    notifyListeners();
+  }
+
+  void selectColor(Color selectedColor) {
+    currentColor = selectedColor;
     notifyListeners();
   }
 }
