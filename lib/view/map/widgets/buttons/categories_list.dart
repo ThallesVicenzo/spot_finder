7c4e7d5694144c956bb123419a4de8003c29e335
Thalles Vicenzo Costa <thalles.vicenzo.costa@gmail.com>
2023-04-26 12:17:43 -0300

    feat: create CategoriesList

---
+++ lib/view/map/widgets/buttons/categories_list.dart
@@ -0,0 +1,40 @@
+import 'package:flutter/material.dart';
+import 'package:provider/provider.dart';
+import 'package:spot_finder/model/models/categories.dart';
+
+import '../../../../view-model/providers/save_spot_provider.dart';
+
+class CategoriesList extends StatelessWidget {
+  const CategoriesList(
+      {super.key, required this.categories, required this.index});
+
+  final Categories categories;
+  final int index;
+  @override
+  Widget build(BuildContext context) {
+    final viewModel = Provider.of<SaveSpotProvider>(context);
+
+    return InkWell(
+      onTap: () {
+        viewModel.onTap(index);
+      },
+      child: Container(
+        decoration: BoxDecoration(
+          border: Border.all(
+            color: viewModel.changeCategorieButtonColor(context, index),
+          ),
+          borderRadius: BorderRadius.circular(15),
+        ),
+        child: Center(
+          child: Text(
+            categories.categories,
+            style: Theme.of(context)
+                .primaryTextTheme
+                .bodySmall!
+                .copyWith(fontSize: 12),
+          ),
+        ),
+      ),
+    );
+  }
+}
