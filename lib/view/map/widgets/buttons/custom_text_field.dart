3592e1537f4360f257029e9ecedf1c6a62cfa482
Thalles Vicenzo Costa <thalles.vicenzo.costa@gmail.com>
2023-04-27 19:40:27 -0300

    feat: update CustomTextField to show error text correctly

--- lib/view/map/widgets/buttons/custom_text_field.dart
+++ lib/view/map/widgets/buttons/custom_text_field.dart
@@ -1,18 +1,23 @@
 import 'package:flutter/material.dart';
-import 'package:spot_finder/view-model/providers/save_spot_provider.dart';
 import 'package:spot_finder/shared/constants.dart';
 
 class CustomTextField extends StatelessWidget {
   const CustomTextField({
     super.key,
     required this.title,
+    required this.value,
+    required this.controller,
     this.onTap,
     this.sufixIcon,
     this.iconColor = const Color(0xFFF44336),
     this.readOnly = false,
+    this.errorText,
   });
 
   final String title;
+  final String value;
+  final TextEditingController? controller;
+  final String? errorText;
   final IconData? sufixIcon;
   final Color iconColor;
   final VoidCallback? onTap;
@@ -20,39 +25,35 @@ class CustomTextField extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    SaveSpotProvider provider = SaveSpotProvider();
-
     return Padding(
       padding: const EdgeInsets.symmetric(vertical: 20),
       child: TextField(
-        controller: provider.textController,
-        readOnly: readOnly,
-        decoration: InputDecoration(
-          labelText: title,
-          labelStyle: Theme.of(context).primaryTextTheme.bodySmall,
-          suffixIcon: Icon(
-            sufixIcon,
-            color: iconColor,
-            size: 30,
-          ),
-          border: kInputBorder.copyWith(
-            borderSide: BorderSide(
-              color: Theme.of(context).primaryColor,
-              width: 2,
+          controller: controller,
+          readOnly: readOnly,
+          decoration: InputDecoration(
+            labelText: title,
+            labelStyle: Theme.of(context).primaryTextTheme.bodySmall,
+            errorText: errorText,
+            suffixIcon: Icon(
+              sufixIcon,
+              color: iconColor,
+              size: 30,
+            ),
+            border: kInputBorder.copyWith(
+              borderSide: BorderSide(
+                color: Theme.of(context).primaryColor,
+                width: 2,
+              ),
+            ),
+            focusedBorder: kInputBorder.copyWith(
+              borderSide: BorderSide(
+                color: Theme.of(context).primaryColor,
+                width: 2,
+              ),
             ),
           ),
-          focusedBorder: kInputBorder.copyWith(
-            borderSide: BorderSide(
-              color: Theme.of(context).primaryColor,
-              width: 2,
-            ),
-          ),
-        ),
-        onTap: onTap,
-        onChanged: (value) {
-          provider.onTextFieldChanged(value);
-        },
-      ),
+          onTap: onTap,
+          onChanged: (value) => value = value),
     );
   }
 }
