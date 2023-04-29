40d6069a57e216bb98de4674b5d6616db011f63c
Thalles Vicenzo Costa <thalles.vicenzo.costa@gmail.com>
2023-04-28 21:15:50 -0300

    feat: update List to change Category field in PopUp

--- lib/view/map/widgets/categories_list.dart
+++ lib/view/map/widgets/categories_list.dart
@@ -2,6 +2,7 @@ import 'package:flutter/material.dart';
 import 'package:provider/provider.dart';
 import 'package:spot_finder/model/models/categories.dart';
 import 'package:spot_finder/view-model/providers/save_spot_provider.dart';
+import 'package:spot_finder/view-model/providers/text_field_provider.dart';
 
 class CategoriesList extends StatelessWidget {
   const CategoriesList({
@@ -14,16 +15,20 @@ class CategoriesList extends StatelessWidget {
   final int index;
   @override
   Widget build(BuildContext context) {
-    final viewModel = Provider.of<SaveSpotProvider>(context);
+    final saveSpotProvider = Provider.of<SaveSpotProvider>(context);
+    final textFieldProvider = Provider.of<TextFieldProvider>(context);
 
     return InkWell(
       onTap: () {
-        viewModel.onCategoryButtonTap(index);
+        saveSpotProvider.onCategoryButtonTap(index);
+        saveSpotProvider.category = categories.categories;
+        textFieldProvider.updateCategoryValue(saveSpotProvider.category);
       },
       child: Container(
         decoration: BoxDecoration(
           border: Border.all(
-            color: viewModel.changeCategorieButtonColor(context, index),
+            color: saveSpotProvider.changeCategorieButtonColor(context, index),
+            width: 2,
           ),
           borderRadius: BorderRadius.circular(15),
         ),
