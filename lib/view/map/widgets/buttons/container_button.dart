43fb9bc577e463eba9cb0832c96f98ff66876600
Thalles Vicenzo Costa <thalles.vicenzo.costa@gmail.com>
2023-04-28 21:09:38 -0300

    style: rename ConfirmButton to ContainerButton

--- lib/view/map/widgets/buttons/container_button.dart
+++ lib/view/map/widgets/buttons/container_button.dart
@@ -1,10 +1,15 @@
 import 'package:flutter/material.dart';
 
-class ConfirmButton extends StatelessWidget {
-  const ConfirmButton({super.key, required this.title, required this.function});
+class ContainerButton extends StatelessWidget {
+  const ContainerButton(
+      {super.key,
+      required this.title,
+      required this.function,
+      required this.fontSize});
 
   final String title;
   final Function() function;
+  final double fontSize;
 
   @override
   Widget build(BuildContext context) {
@@ -15,6 +20,7 @@ class ConfirmButton extends StatelessWidget {
           padding: const EdgeInsets.symmetric(vertical: 10),
           child: Container(
             height: 50,
+            width: MediaQuery.of(context).size.width * 0.82,
             decoration: BoxDecoration(
               color: Theme.of(context).highlightColor,
               borderRadius: BorderRadius.circular(2),
@@ -22,10 +28,10 @@ class ConfirmButton extends StatelessWidget {
             child: Center(
               child: Text(
                 title,
-                style: Theme.of(context)
-                    .primaryTextTheme
-                    .bodySmall!
-                    .copyWith(fontSize: 20),
+                textAlign: TextAlign.center,
+                style: Theme.of(context).primaryTextTheme.bodySmall!.copyWith(
+                      fontSize: fontSize,
+                    ),
               ),
             ),
           ),
