faf26ba414c0e018bbbd046a7c5efa961a1bbbc2
Thalles Vicenzo Costa <thalles.vicenzo.costa@gmail.com>
2023-05-01 21:30:54 -0300

    feat: create LocationProvider on app builder

--- lib/main.dart
+++ lib/main.dart
@@ -1,6 +1,7 @@
 import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
 import 'package:provider/provider.dart';
+import 'package:spot_finder/view-model/providers/location_provider.dart';
 import 'package:spot_finder/view-model/providers/text_field_provider.dart';
 
 import 'shared/theme.dart';
@@ -18,6 +19,7 @@ void main() async {
         ChangeNotifierProvider(create: (_) => MarkersProvider()),
         ChangeNotifierProvider(create: (_) => SaveSpotProvider()),
         ChangeNotifierProvider(create: (_) => TextFieldProvider()),
+        ChangeNotifierProvider(create: (_) => LocationProvider()),
       ],
       child: const MapHelper(),
     ),
