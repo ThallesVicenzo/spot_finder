4619ecdb68a504af4a415028f5cb81ec9edc9c56
Thalles Vicenzo Costa <thalles.vicenzo.costa@gmail.com>
2023-05-01 21:32:43 -0300

    refactor: some changes to consume LocationProvider

--- lib/view-model/providers/markers_provider.dart
+++ lib/view-model/providers/markers_provider.dart
@@ -1,29 +1,34 @@
+import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_map/flutter_map.dart';
 import 'package:latlong2/latlong.dart';
-
-import '../../view/map/map_screen.dart';
+import 'package:provider/provider.dart';
+import 'package:spot_finder/view-model/providers/location_provider.dart';
+import 'package:spot_finder/view-model/providers/save_spot_provider.dart';
 
 class MarkersProvider extends ChangeNotifier {
-  List<Marker> markers = [
-    Marker(
-        point: LatLng(location.latitude, location.longitude),
-        builder: (context) => const Icon(
-              Icons.pin_drop_sharp,
-              color: Colors.red,
-              size: 40,
-            )),
-  ];
+  List<Marker> getMarkersList(context) {
+    double latitude = Provider.of<LocationProvider>(context).latitude;
+    double longitude = Provider.of<LocationProvider>(context).longitude;
+    List<Marker> markers = [
+      Marker(
+          point: LatLng(latitude, longitude),
+          builder: (context) => const Icon(
+                Icons.pin_drop_sharp,
+                color: Colors.red,
+                size: 40,
+              )),
+    ];
+    return markers;
+  }
 
-  List<Marker> get markersList => markers;
-
-  void addMarkers(double lat, double long) {
-    markersList.add(
+  void addMarkers(double lat, double long, context) {
+    getMarkersList(context).add(
       Marker(
         point: LatLng(lat, long),
-        builder: (context) => const Icon(
+        builder: (context) => Icon(
           Icons.pin_drop_sharp,
-          color: Colors.blue,
+          color: Provider.of<SaveSpotProvider>(context).currentColor,
           size: 40,
         ),
       ),
--- lib/view/map/map_screen.dart
+++ lib/view/map/map_screen.dart
@@ -2,7 +2,9 @@ import 'package:flutter/material.dart';
 import 'package:flutter_map/flutter_map.dart';
 import 'package:latlong2/latlong.dart';
 import 'package:provider/provider.dart';
-import 'package:spot_finder/model/services/location.dart';
+import 'package:spot_finder/view-model/providers/location_provider.dart';
+
+import '../../view/map/loading_screen.dart';
 
 import '../../../view-model/providers/markers_provider.dart';
 import '../../shared/constants.dart';
@@ -15,60 +17,53 @@ class MapScreen extends StatefulWidget {
   State<MapScreen> createState() => _MapScreenState();
 }
 
-final Location location = Location();
-final MapController mapController = MapController();
-
-Future<void> getLocation() async {
-  return await location.determinePosition();
-}
-
 class _MapScreenState extends State<MapScreen> {
   MarkersProvider markersProvider = MarkersProvider();
 
   @override
   Widget build(BuildContext context) {
+    final location = Provider.of<LocationProvider>(context);
+
     return Scaffold(
       resizeToAvoidBottomInset: false,
       body: FutureBuilder(
-          initialData: getLocation(),
+          future: location.getPosition(),
           builder: (BuildContext context, AsyncSnapshot<dynamic> snapshot) {
+            if (snapshot.connectionState == ConnectionState.waiting) {
+              return const LoadingScreen();
+            }
             return Column(
               children: [
                 Flexible(
-                  child: ChangeNotifierProvider(
-                    create: (_) => markersProvider,
-                    child: Consumer<MarkersProvider>(
-                      builder: (_, markersProvider, __) => FlutterMap(
-                        options: MapOptions(
-                          center: LatLng(location.latitude, location.longitude),
-                          zoom: 18.0,
-                          onTap: (tapPosition, point) {
-                            showDialog(
-                                barrierDismissible: false,
-                                context: context,
-                                builder: (_) {
-                                  return const Padding(
-                                    padding: EdgeInsets.symmetric(
-                                      horizontal: 15,
-                                      vertical: 70,
-                                    ),
-                                    child: SaveSpot(),
-                                  );
-                                });
-                          },
-                        ),
-                        children: [
-                          TileLayer(
-                            urlTemplate:
-                                "https://api.mapbox.com/styles/v1/mapbox/streets-v12/tiles/{z}/{x}/{y}?access_token=$kAccessToken",
-                            subdomains: const ['a', 'b', 'c'],
-                          ),
-                          MarkerLayer(
-                            markers: markersProvider.markers,
-                          )
-                        ],
-                      ),
+                  child: FlutterMap(
+                    options: MapOptions(
+                      center: LatLng(location.latitude, location.longitude),
+                      zoom: 18.0,
+                      onTap: (tapPosition, point) {
+                        showDialog(
+                            barrierDismissible: false,
+                            context: context,
+                            builder: (_) {
+                              return const Padding(
+                                padding: EdgeInsets.symmetric(
+                                  horizontal: 15,
+                                  vertical: 70,
+                                ),
+                                child: SaveSpot(),
+                              );
+                            });
+                      },
                     ),
+                    children: [
+                      TileLayer(
+                        urlTemplate:
+                            "https://api.mapbox.com/styles/v1/mapbox/streets-v12/tiles/{z}/{x}/{y}?access_token=$kAccessToken",
+                        subdomains: const ['a', 'b', 'c'],
+                      ),
+                      MarkerLayer(
+                        markers: markersProvider.getMarkersList(context),
+                      )
+                    ],
                   ),
                 ),
               ],
