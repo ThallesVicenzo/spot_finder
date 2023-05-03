62b3cd3486591f9769c210b432115e2b7ef42b28
Thalles Vicenzo Costa <thalles.vicenzo.costa@gmail.com>
2023-05-03 20:51:26 -0300

    refactor: remove unecessary data & add toString method

--- lib/model/models/places.dart
+++ lib/model/models/places.dart
@@ -1,25 +1,17 @@
-import 'package:spot_finder/model/models/structured_formatting.dart';
-
 class PlacesModel {
   final String? description;
-  final StructuredFormattingModel? structuredFormating;
-  final String? placeId;
   final String? reference;
 
-  PlacesModel(
-      {this.structuredFormating,
-      this.placeId,
-      this.reference,
-      this.description});
+  PlacesModel({this.reference, this.description});
 
   factory PlacesModel.fromJson(Map<String, dynamic> json) {
     return PlacesModel(
       description: json['description'] as String?,
-      placeId: json['place_id'] as String?,
       reference: json['reference'] as String?,
-      structuredFormating: json['strutured_formatting'] != null
-          ? StructuredFormattingModel.fromJson(json['strutured_formatting'])
-          : null,
     );
+  }
+  @override
+  String toString() {
+    return '$description, $reference';
   }
 }
