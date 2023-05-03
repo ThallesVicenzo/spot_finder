b7226b6107191edf7c2287cdeff6911b970d1a09
Thalles Vicenzo Costa <thalles.vicenzo.costa@gmail.com>
2023-05-03 20:53:15 -0300

    feat: create PlacesProvider

---
+++ lib/view-model/providers/places_provider.dart
@@ -0,0 +1,17 @@
+import 'package:flutter/material.dart';
+import 'package:spot_finder/model/models/places.dart';
+import 'package:spot_finder/view-model/repository/places_repository.dart';
+
+class PlacesProvider with ChangeNotifier {
+  final PlacesRepository _repository = PlacesRepository();
+  List<PlacesModel>? list = [];
+
+  Future<void> showPredictions(String query) async {
+    await _repository.defineUri(query).then(
+      (value) {
+        list = value;
+        notifyListeners();
+      },
+    );
+  }
+}
