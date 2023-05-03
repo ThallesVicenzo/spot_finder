05a59a50b03cbcf6697edca126a52106d48d517e
Thalles Vicenzo Costa <thalles.vicenzo.costa@gmail.com>
2023-05-03 16:11:28 -0300

    feat: call disableButtons on screen

--- lib/view/authentication/authentication_screen.dart
+++ lib/view/authentication/authentication_screen.dart
@@ -18,67 +18,72 @@ class _AuthenticationScreen extends State<AuthenticationScreen> {
     return ChangeNotifierProvider(
       create: (_) => authenticationProvider,
       child: Consumer<AuthenticationProvider>(
-        builder: (_, authenticationProvider, __) => Stack(
-          children: [
-            SizedBox(
-              width: MediaQuery.of(context).size.width,
-              height: MediaQuery.of(context).size.height,
-              child: Image.asset(
-                kBackground,
-                fit: BoxFit.fill,
-              ),
-            ),
-            Center(
-              child: Container(
-                width: MediaQuery.of(context).size.width * 0.78,
-                height: MediaQuery.of(context).size.height * 0.625,
-                decoration: BoxDecoration(
-                  color: Theme.of(context).splashColor,
-                  borderRadius: BorderRadius.circular(12),
+        builder: (_, authenticationProvider, __) => Material(
+          child: Stack(
+            children: [
+              SizedBox(
+                width: MediaQuery.of(context).size.width,
+                height: MediaQuery.of(context).size.height,
+                child: Image.asset(
+                  kBackground,
+                  fit: BoxFit.fill,
                 ),
-                child: Column(
-                  mainAxisAlignment: MainAxisAlignment.spaceEvenly,
-                  children: [
-                    Text(
-                      'Place Rater',
-                      style: Theme.of(context).primaryTextTheme.bodyLarge,
-                    ),
-                    Padding(
-                      padding: const EdgeInsets.symmetric(horizontal: 15),
-                      child: Column(
-                        children: [
-                          AuthenticationButton(
-                            title: 'Continuar com o Facebook',
-                            icon: kFacebookSignIn,
-                            style: Theme.of(context)
-                                .primaryTextTheme
-                                .bodySmall!
-                                .copyWith(color: Theme.of(context).splashColor),
-                            backGroundColor: Theme.of(context).primaryColor,
-                            function: () {
-                              authenticationProvider
-                                  .signInWithFacebook(context);
-                            },
-                          ),
-                          AuthenticationButton(
-                            title: 'Continuar com o google',
-                            icon: kGoogleSignIn,
-                            style: Theme.of(context)
-                                .primaryTextTheme
-                                .bodySmall!
-                                .copyWith(
-                                    color:
-                                        Theme.of(context).secondaryHeaderColor),
-                            function: () {},
-                          )
-                        ],
+              ),
+              Center(
+                child: Container(
+                  width: MediaQuery.of(context).size.width * 0.78,
+                  height: MediaQuery.of(context).size.height * 0.625,
+                  decoration: BoxDecoration(
+                    color: Theme.of(context).splashColor,
+                    borderRadius: BorderRadius.circular(12),
+                  ),
+                  child: Column(
+                    mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+                    children: [
+                      Text(
+                        'Place Rater',
+                        style: Theme.of(context).primaryTextTheme.bodyLarge,
                       ),
-                    )
-                  ],
+                      Padding(
+                        padding: const EdgeInsets.symmetric(horizontal: 15),
+                        child: Column(
+                          children: [
+                            AuthenticationButton(
+                              title: 'Continuar com o Facebook',
+                              disable: authenticationProvider.disable,
+                              icon: kFacebookSignIn,
+                              style: Theme.of(context)
+                                  .primaryTextTheme
+                                  .bodySmall!
+                                  .copyWith(
+                                      color: Theme.of(context).splashColor),
+                              backGroundColor: Theme.of(context).primaryColor,
+                              function: () {
+                                authenticationProvider
+                                    .signInWithFacebook(context);
+                              },
+                            ),
+                            AuthenticationButton(
+                              title: 'Continuar com o google',
+                              disable: authenticationProvider.disable,
+                              icon: kGoogleSignIn,
+                              style: Theme.of(context)
+                                  .primaryTextTheme
+                                  .bodySmall!
+                                  .copyWith(
+                                      color: Theme.of(context)
+                                          .secondaryHeaderColor),
+                              function: () {},
+                            )
+                          ],
+                        ),
+                      )
+                    ],
+                  ),
                 ),
-              ),
-            )
-          ],
+              )
+            ],
+          ),
         ),
       ),
     );
