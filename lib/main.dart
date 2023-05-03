53774221916a410bfb28a12fab1588d61e5d1ecc
Thalles Vicenzo Costa <thalles.vicenzo.costa@gmail.com>
2023-05-03 16:07:01 -0300

    feat: create PlacesProvider on main

--- lib/main.dart
+++ lib/main.dart
@@ -2,6 +2,7 @@ import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
 import 'package:provider/provider.dart';
 import 'package:spot_finder/view-model/providers/location_provider.dart';
+import 'package:spot_finder/view-model/providers/places_provider.dart';
 import 'package:spot_finder/view-model/providers/text_field_provider.dart';
 
 import 'shared/theme.dart';
@@ -20,6 +21,7 @@ void main() async {
         ChangeNotifierProvider(create: (_) => SaveSpotProvider()),
         ChangeNotifierProvider(create: (_) => TextFieldProvider()),
         ChangeNotifierProvider(create: (_) => LocationProvider()),
+        ChangeNotifierProvider(create: (_) => PlacesProvider()),
       ],
       child: const MapHelper(),
     ),
