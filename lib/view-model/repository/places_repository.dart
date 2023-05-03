fad145047772b3114d69be4507c30d7523bb7480
Thalles Vicenzo Costa <thalles.vicenzo.costa@gmail.com>
2023-05-03 20:54:11 -0300

    refactor: make class convert & return a List<PlacesModel>

--- lib/view-model/repository/places_repository.dart
+++ lib/view-model/repository/places_repository.dart
@@ -1,22 +1,25 @@
+import 'package:spot_finder/model/models/places.dart';
 import 'package:spot_finder/model/services/google_places_service.dart';
 
-import '../../model/models/places.dart';
+import '../../model/models/places_auto_complete.dart';
 import '../../shared/constants.dart';
 
 class PlacesRepository {
-  dynamic defineUri(String query) async {
+  Future<List<PlacesModel>?> defineUri(String query) async {
     Uri uri =
         Uri.https('maps.googleapis.com', 'maps/api/place/autocomplete/json', {
       'input': query,
       'key': kGooglePlacesApiKey,
     });
+
     String? response = await GooglePlacesService.fetchUrl(uri);
     if (response != null) {
-      PlacesAutoCompleteModel result =
-          PlacesAutoCompleteModel.placesAutoCompleteModel(response);
-      if (result.predictions != null) {
-        return result.predictions;
-      }
+      List<PlacesModel> result =
+          PlacesAutoCompleteModel.parseAutoCompleteModel(response)
+              .predictions!
+              .toList();
+      return result;
     }
+    return null;
   }
 }
