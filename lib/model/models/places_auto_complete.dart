d1a41a4e3a672602130baef5ff6645f31bfb7845
Thalles Vicenzo Costa <thalles.vicenzo.costa@gmail.com>
2023-05-03 20:52:17 -0300

    fix: fix logic in 'predictions'

--- lib/model/models/places_auto_complete.dart
+++ lib/model/models/places_auto_complete.dart
@@ -1,20 +1,23 @@
 import 'dart:convert';
 
+import 'package:spot_finder/model/models/places.dart';
+
 class PlacesAutoCompleteModel {
   final String? status;
-  final List<PlaceModel>? predictions;
+  final List<PlacesModel>? predictions;
 
   PlacesAutoCompleteModel({this.status, this.predictions});
 
   factory PlacesAutoCompleteModel.fromJson(Map<String, dynamic> json) {
     return PlacesAutoCompleteModel(
         status: json['status'] as String?,
-        predictions: json['predictions'] != null
-            ? json['predictions']
-                .map<PlacesModel>((json) => PlacesModel.fromJson(json).toList())
-            : null);
+        predictions: List<PlacesModel>.from(
+          json['predictions']?.map<PlacesModel>(
+            (args) => PlacesModel.fromJson(args),
+          ),
+        ));
   }
-  static PlacesAutoCompleteModel placesAutoCompleteModel(String responseBody) {
+  static PlacesAutoCompleteModel parseAutoCompleteModel(String responseBody) {
     final parsed = json.decode(responseBody).cast<String, dynamic>();
     return PlacesAutoCompleteModel.fromJson(parsed);
   }
