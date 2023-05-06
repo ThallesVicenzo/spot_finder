b5fc8ccea7e8a32d580170440c6290da23edb6a6
Thalles Vicenzo Costa <thalles.vicenzo.costa@gmail.com>
2023-05-05 22:14:50 -0300

    refactor: create Consumer

--- lib/view/camera/camera_screen.dart
+++ lib/view/camera/camera_screen.dart
@@ -13,77 +13,70 @@ class CameraScreen extends StatefulWidget {
 }
 
 class _CameraScreenState extends State<CameraScreen> {
-  final camera = CameraProvider();
-
   @override
-  void initState() {
-    super.initState();
-    camera.initCamera(widget.cameras![0], mounted);
-  }
+  void didChangeDependencies() {
+    Provider.of<CameraProvider>(context, listen: false)
+        .initCamera(widget.cameras![0], mounted);
 
-  @override
-  void dispose() {
-    camera.cameraController.dispose();
-    super.dispose();
+    super.didChangeDependencies();
   }
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       body: SafeArea(
-        child: ChangeNotifierProvider(
-          create: (_) => camera,
-          child: Consumer<CameraProvider>(
-            builder: (_, markersProvider, __) => Stack(
-              children: [
-                (camera.cameraController.value.isInitialized)
-                    ? CameraPreview(camera.cameraController)
-                    : Container(
-                        color: Colors.black,
-                        child:
-                            const Center(child: CircularProgressIndicator())),
-                Align(
-                  alignment: Alignment.bottomCenter,
-                  child: Container(
-                    height: MediaQuery.of(context).size.height * 0.20,
-                    decoration: const BoxDecoration(
-                        borderRadius:
-                            BorderRadius.vertical(top: Radius.circular(24)),
-                        color: Colors.black),
-                    child: Row(
-                      crossAxisAlignment: CrossAxisAlignment.center,
-                      children: [
-                        const Spacer(),
-                        Expanded(
-                          child: IconButton(
-                            onPressed: () {
-                              camera.takePicture(context);
-                            },
-                            iconSize: 50,
-                            padding: EdgeInsets.zero,
-                            constraints: const BoxConstraints(),
-                            icon: const Icon(Icons.circle, color: Colors.white),
+        child: Consumer<CameraProvider>(
+          builder: (_, cameraProvider, __) => Stack(
+            children: [
+              (cameraProvider.cameraController.value.isInitialized)
+                  ? CameraPreview(cameraProvider.cameraController)
+                  : Container(
+                      color: Colors.black,
+                      child: const Center(
+                        child: CircularProgressIndicator(),
+                      ),
+                    ),
+              Align(
+                alignment: Alignment.bottomCenter,
+                child: Container(
+                  height: MediaQuery.of(context).size.height * 0.20,
+                  decoration: const BoxDecoration(
+                      borderRadius:
+                          BorderRadius.vertical(top: Radius.circular(24)),
+                      color: Colors.black),
+                  child: Row(
+                    crossAxisAlignment: CrossAxisAlignment.center,
+                    children: [
+                      const Spacer(),
+                      Expanded(
+                        child: IconButton(
+                          onPressed: () {
+                            cameraProvider.takePicture(context);
+                          },
+                          iconSize: 50,
+                          padding: EdgeInsets.zero,
+                          constraints: const BoxConstraints(),
+                          icon: const Icon(Icons.circle, color: Colors.white),
+                        ),
+                      ),
+                      Expanded(
+                        child: IconButton(
+                          onPressed: () {
+                            cameraProvider.changeFlashIcon();
+                          },
+                          icon: Icon(
+                            cameraProvider.isFlashButtonClicked
+                                ? Icons.flash_on
+                                : Icons.flash_off,
+                            color: Colors.white,
                           ),
                         ),
-                        Expanded(
-                          child: IconButton(
-                            onPressed: () {
-                              camera.changeFlashIcon();
-                            },
-                            icon: Icon(
-                              camera.isFlashButtonClicked
-                                  ? Icons.flash_on
-                                  : Icons.flash_off,
-                              color: Colors.white,
-                            ),
-                          ),
-                        )
-                      ],
-                    ),
+                      )
+                    ],
                   ),
                 ),
-              ],
-            ),
+              ),
+            ],
           ),
         ),
       ),
