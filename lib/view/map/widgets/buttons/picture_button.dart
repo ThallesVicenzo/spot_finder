ceb7981bda3502a291031ccf7d8bf81e14267324
Thalles Vicenzo Costa <thalles.vicenzo.costa@gmail.com>
2023-05-05 22:16:35 -0300

    refactor: create Consumer

--- lib/view/map/widgets/buttons/picture_button.dart
+++ lib/view/map/widgets/buttons/picture_button.dart
@@ -1,45 +1,53 @@
+import 'package:cross_file_image/cross_file_image.dart';
 import 'package:flutter/material.dart';
+import 'package:provider/provider.dart';
 
 import '../../../../view-model/providers/camera_provider.dart';
 
 class PictureButton extends StatelessWidget {
-  PictureButton({
+  const PictureButton({
     super.key,
   });
-
-  final CameraProvider camera = CameraProvider();
 
   @override
   Widget build(BuildContext context) {
     return Material(
-      child: InkWell(
-        onTap: () {
-          CameraProvider().goToPictureScreen(context);
-        },
-        child: SizedBox(
-          height: MediaQuery.of(context).size.height * 0.2,
-          child: Container(
-            color: Theme.of(context).cardColor,
-            child: camera.isPictureTaken
-                ? Stack(
-                    children: [
-                      Image(image: camera.displayImage!),
-                      Center(
-                        child: Icon(
-                          Icons.camera_alt,
-                          color: Theme.of(context).primaryColor,
-                          size: 45,
+      child: Consumer<CameraProvider>(
+        builder: (context, cameraProvider, child) => InkWell(
+          onTap: () {
+            CameraProvider().goToPictureScreen(context);
+          },
+          child: SizedBox(
+            height: MediaQuery.of(context).size.height * 0.2,
+            child: Container(
+              color: Theme.of(context).cardColor,
+              child: cameraProvider.isPictureTaken
+                  ? Stack(
+                      children: [
+                        Center(
+                          child: Image(
+                            image:
+                                XFileImage(cameraProvider.picture!, scale: 0.1),
+                            height: MediaQuery.of(context).size.height * 0.2,
+                          ),
                         ),
+                        Center(
+                          child: Icon(
+                            Icons.camera_alt,
+                            color: Theme.of(context).primaryColor,
+                            size: 45,
+                          ),
+                        ),
+                      ],
+                    )
+                  : Center(
+                      child: Icon(
+                        Icons.camera_alt,
+                        size: 45,
+                        color: Theme.of(context).primaryColor,
                       ),
-                    ],
-                  )
-                : Center(
-                    child: Icon(
-                      Icons.camera_alt,
-                      size: 45,
-                      color: Theme.of(context).primaryColor,
                     ),
-                  ),
+            ),
           ),
         ),
       ),
