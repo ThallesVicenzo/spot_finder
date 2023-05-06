73d7f5774ccf5e2334923e92c0af4ceeaf79b736
Thalles Vicenzo Costa <thalles.vicenzo.costa@gmail.com>
2023-05-05 22:12:31 -0300

    refactor: call locationProvider

--- lib/view-model/providers/markers_provider.dart
+++ lib/view-model/providers/markers_provider.dart
@@ -1,15 +1,15 @@
-import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_map/flutter_map.dart';
 import 'package:latlong2/latlong.dart';
 import 'package:provider/provider.dart';
 import 'package:spot_finder/view-model/providers/location_provider.dart';
-import 'package:spot_finder/view-model/providers/save_spot_provider.dart';
 
 class MarkersProvider extends ChangeNotifier {
   List<Marker> getMarkersList(context) {
-    double latitude = Provider.of<LocationProvider>(context).latitude;
-    double longitude = Provider.of<LocationProvider>(context).longitude;
+    final locationProvider = Provider.of<LocationProvider>(context);
+
+    double latitude = locationProvider.latitude;
+    double longitude = locationProvider.longitude;
     List<Marker> markers = [
       Marker(
           point: LatLng(latitude, longitude),
@@ -22,17 +22,17 @@ class MarkersProvider extends ChangeNotifier {
     return markers;
   }
 
-  void addMarkers(double lat, double long, context) {
-    getMarkersList(context).add(
-      Marker(
-        point: LatLng(lat, long),
-        builder: (context) => Icon(
-          Icons.pin_drop_sharp,
-          color: Provider.of<SaveSpotProvider>(context).currentColor,
-          size: 40,
-        ),
-      ),
-    );
-    notifyListeners();
-  }
+  // void addMarkers(double lat, double long, context) {
+  //   getMarkersList(context).add(
+  //     Marker(
+  //       point: LatLng(lat, long),
+  //       builder: (context) => Icon(
+  //         Icons.pin_drop_sharp,
+  //         color: Provider.of<SaveSpotProvider>(context).currentColor,
+  //         size: 40,
+  //       ),
+  //     ),
+  //   );
+  //   notifyListeners();
+  // }
 }
