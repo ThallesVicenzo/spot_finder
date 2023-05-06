e3f4569668f5b5246268a24ef776ab9dfcb2dc55
Thalles Vicenzo Costa <thalles.vicenzo.costa@gmail.com>
2023-05-05 22:19:14 -0300

    refactor: call providers on constructor

--- lib/view/map/widgets/categories/categories_builder.dart
+++ lib/view/map/widgets/categories/categories_builder.dart
@@ -1,16 +1,23 @@
 import 'package:flutter/material.dart';
-import 'package:provider/provider.dart';
 import 'package:spot_finder/view-model/providers/save_spot_provider.dart';
 import 'package:spot_finder/view-model/providers/text_field_provider.dart';
 import 'package:spot_finder/view/map/widgets/buttons/container_button.dart';
 import 'package:spot_finder/view/map/widgets/buttons/custom_alert_dialog.dart';
+import '../../../../view-model/providers/categories_provider.dart';
 import '../buttons/custom_text_field.dart';
 import 'categories_list.dart';
 
 class CategoriesBuilder extends StatefulWidget {
   const CategoriesBuilder({
+    required this.categoriesProvider,
+    required this.textFieldProvider,
+    required this.saveSpotProvider,
     super.key,
   });
+
+  final CategoriesProvider categoriesProvider;
+  final SaveSpotProvider saveSpotProvider;
+  final TextFieldProvider textFieldProvider;
 
   @override
   State<CategoriesBuilder> createState() => _CategoriesBuilderState();
@@ -19,9 +26,6 @@ class CategoriesBuilder extends StatefulWidget {
 class _CategoriesBuilderState extends State<CategoriesBuilder> {
   @override
   Widget build(BuildContext context) {
-    final saveSpotProvider = Provider.of<SaveSpotProvider>(context);
-    final textFieldProvider = Provider.of<TextFieldProvider>(context);
-
     return SingleChildScrollView(
       child: Column(
         mainAxisSize: MainAxisSize.min,
@@ -30,7 +34,7 @@ class _CategoriesBuilderState extends State<CategoriesBuilder> {
             height: MediaQuery.of(context).size.height * 0.2,
             width: MediaQuery.of(context).size.width,
             child: GridView.builder(
-                itemCount: saveSpotProvider.categoriesModel.length,
+                itemCount: widget.categoriesProvider.categoriesModel.length,
                 gridDelegate: const SliverGridDelegateWithFixedCrossAxisCount(
                     crossAxisCount: 3,
                     mainAxisSpacing: 20,
@@ -39,7 +43,10 @@ class _CategoriesBuilderState extends State<CategoriesBuilder> {
                 itemBuilder: (context, index) {
                   return CategoriesList(
                     index: index,
-                    categories: saveSpotProvider.categoriesModel[index],
+                    categories:
+                        widget.categoriesProvider.categoriesModel[index],
+                    saveSpotProvider: widget.saveSpotProvider,
+                    textFieldProvider: widget.textFieldProvider,
                   );
                 }),
           ),
@@ -55,10 +62,9 @@ class _CategoriesBuilderState extends State<CategoriesBuilder> {
                           title: 'Escreva uma nova categoria.',
                           content: CustomTextField(
                             title: 'Nova categoria.',
-                            errorText: textFieldProvider.returnError,
-                            controller:
-                                textFieldProvider.textEditingControllers[1],
-                            onChangedValue: saveSpotProvider.category,
+                            errorText: widget.textFieldProvider.returnError,
+                            controller: widget
+                                .textFieldProvider.textEditingControllers[1],
                           ),
                           function: () {
                             Navigator.pop(context);
