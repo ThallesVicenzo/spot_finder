d8ef52fcaaaa57a27a08789455f0180b45630fdf
Thalles Vicenzo Costa <thalles.vicenzo.costa@gmail.com>
2023-05-09 18:53:48 -0300

    feat: create saveSpot & newCategory methods

--- lib/model/services/firebase_service.dart
+++ lib/model/services/firebase_service.dart
@@ -1,10 +1,27 @@
 import 'package:cloud_firestore/cloud_firestore.dart';
 
 class FirebaseService {
-  static FirebaseFirestore instance = FirebaseFirestore.instance;
+  static final _instance = FirebaseFirestore.instance;
 
   static CollectionReference<Map<String, dynamic>> getCollectionsSnapshot(
       String collection) {
-    return instance.collection(collection);
+    return _instance.collection(collection);
+  }
+
+  static Future<void> saveSpot(
+    String collection,
+    String docName,
+    Map<String, dynamic> data,
+  ) async {
+    await getCollectionsSnapshot(collection).doc(docName).set(data);
+  }
+
+  static Future<void> newCategory(
+    String docName,
+    Map<String, dynamic> data,
+  ) async {
+    await getCollectionsSnapshot('Categories')
+        .doc(docName.toLowerCase())
+        .set(data);
   }
 }
