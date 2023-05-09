7359f7ddbb6ee173ea86ea756147f3787d3b4403
Thalles Vicenzo Costa <thalles.vicenzo.costa@gmail.com>
2023-05-09 18:53:03 -0300

    feat: create toJson method

--- lib/model/models/categories.dart
+++ lib/model/models/categories.dart
@@ -1,11 +1,13 @@
-import 'package:cloud_firestore/cloud_firestore.dart';
-
 class CategoriesModel {
   const CategoriesModel({required this.categories});
   final String categories;
 
   factory CategoriesModel.fromJson(
-    DocumentSnapshot<Map<String, dynamic>> map,
+    Map<String, dynamic> map,
   ) =>
       CategoriesModel(categories: map['Categoria']);
+
+  Map<String, dynamic> toJson() => {
+        'Categoria': categories,
+      };
 }
