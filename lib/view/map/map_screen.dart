1b5bfa0d963c8eea9288c570ed7ff6a6e3624642
Thalles Vicenzo Costa <thalles.vicenzo.costa@gmail.com>
2023-05-09 18:56:30 -0300

    refactor: modifications to make MapScreen get spots from firebase

--- lib/view/map/map_screen.dart
+++ lib/view/map/map_screen.dart
@@ -18,64 +18,69 @@ class MapScreen extends StatefulWidget {
 }
 
 class _MapScreenState extends State<MapScreen> {
-  late MarkersProvider markersProvider;
-  late LocationProvider locationProvider;
-
-  @override
-  void didChangeDependencies() {
-    locationProvider = Provider.of<LocationProvider>(context);
-    markersProvider = Provider.of<MarkersProvider>(context);
-    super.didChangeDependencies();
-  }
-
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       resizeToAvoidBottomInset: false,
-      body: FutureBuilder(
-          future: locationProvider.getPosition(),
-          builder: (BuildContext context, AsyncSnapshot<dynamic> snapshot) {
-            if (snapshot.connectionState == ConnectionState.waiting) {
-              return const LoadingScreen();
-            }
-            return Column(
-              children: [
-                Flexible(
-                  child: FlutterMap(
-                    options: MapOptions(
-                      center: LatLng(locationProvider.latitude,
-                          locationProvider.longitude),
-                      zoom: 18.0,
-                      onTap: (tapPosition, point) {
-                        showDialog(
-                            barrierDismissible: false,
-                            context: context,
-                            builder: (_) {
-                              return const Padding(
-                                padding: EdgeInsets.symmetric(
-                                  horizontal: 15,
-                                  vertical: 70,
-                                ),
-                                child: SaveSpot(),
-                              );
-                            });
-                      },
-                    ),
-                    children: [
-                      TileLayer(
-                        urlTemplate:
-                            "https://api.mapbox.com/styles/v1/mapbox/streets-v12/tiles/{z}/{x}/{y}?access_token=$kAccessToken",
-                        subdomains: const ['a', 'b', 'c'],
+      body: Consumer<LocationProvider>(
+        builder: (context, locationProvider, __) => FutureBuilder(
+            future: Provider.of<LocationProvider>(context).getPosition(),
+            builder: (BuildContext context, AsyncSnapshot<dynamic> snapshot) {
+              if (snapshot.connectionState == ConnectionState.waiting) {
+                return const LoadingScreen();
+              }
+              return Column(
+                children: [
+                  Flexible(
+                    child: FlutterMap(
+                      options: MapOptions(
+                        center: LatLng(locationProvider.latitude,
+                            locationProvider.longitude),
+                        zoom: 18.0,
+                        onTap: (tapPosition, point) {
+                          showDialog(
+                              barrierDismissible: false,
+                              context: context,
+                              builder: (_) {
+                                return Padding(
+                                  padding: const EdgeInsets.symmetric(
+                                    horizontal: 15,
+                                    vertical: 70,
+                                  ),
+                                  child: SaveSpot(
+                                    lat: point.latitude,
+                                    long: point.longitude,
+                                  ),
+                                );
+                              });
+                        },
                       ),
-                      MarkerLayer(
-                        markers: markersProvider.getMarkersList(context),
-                      )
-                    ],
+                      children: [
+                        TileLayer(
+                          urlTemplate:
+                              "https://api.mapbox.com/styles/v1/mapbox/streets-v12/tiles/{z}/{x}/{y}?access_token=$kAccessToken",
+                          subdomains: const ['a', 'b', 'c'],
+                        ),
+                        Consumer<MarkersProvider>(
+                          builder: (context, markersProvider, __) =>
+                              FutureBuilder(
+                            future: markersProvider.getTouristSpots(
+                              locationProvider.latitude,
+                              locationProvider.longitude,
+                              context,
+                            ),
+                            builder: (__, _) => MarkerLayer(
+                              markers: markersProvider.getMarkersList(context),
+                            ),
+                          ),
+                        )
+                      ],
+                    ),
                   ),
-                ),
-              ],
-            );
-          }),
+                ],
+              );
+            }),
+      ),
     );
   }
 }
