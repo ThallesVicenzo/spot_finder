2d1e54e08393079a66dd8e8abcb240cd99ac7063
Thalles Vicenzo Costa <thalles.vicenzo.costa@gmail.com>
2023-05-09 18:55:55 -0300

    feat: create getTouristSpotsDocuments, updateCategories & validadeNewCategory functions

--- lib/view-model/repository/firebase_repository.dart
+++ lib/view-model/repository/firebase_repository.dart
@@ -1,3 +1,5 @@
+import 'package:spot_finder/model/models/spots.dart';
+
 import '../../model/models/categories.dart';
 import '../../model/services/firebase_service.dart';
 
@@ -9,10 +11,72 @@ class FirebaseRepository {
       final data =
           await FirebaseService.getCollectionsSnapshot('Categories').get();
       return data.docs.map((docData) {
-        return CategoriesModel.fromJson(docData);
+        return CategoriesModel.fromJson(
+          docData.data(),
+        );
       }).toList();
     } catch (e) {
       throw Exception(e);
     }
+  }
+
+  static Future<List<TouristSpotModel>>? getTouristSpotsDocuments() async {
+    try {
+      final data =
+          await FirebaseService.getCollectionsSnapshot('TouristSpot').get();
+      return data.docs
+          .map((docData) => TouristSpotModel.fromJson(
+                docData.data(),
+              ))
+          .toList();
+    } catch (e) {
+      throw Exception(e);
+    }
+  }
+
+  static Future<void> callSavePlace(
+      {required String collection,
+      required String docName,
+      required String title,
+      required String location,
+      required String description,
+      required String category,
+      required String mainPicture,
+      required String pinColor}) async {
+    try {
+      Map<String, dynamic> data = TouristSpotModel(
+        title: title,
+        location: location,
+        description: description,
+        category: category,
+        mainPicture: mainPicture,
+        pinColor: pinColor,
+      ).toJson();
+
+      await FirebaseService.saveSpot(collection, docName, data);
+    } catch (e) {
+      throw Exception(e);
+    }
+  }
+
+  static Future<void> updateCategories({
+    required String category,
+    required String docName,
+  }) async {
+    try {
+      Map<String, dynamic> data =
+          CategoriesModel(categories: category.toUpperCase()).toJson();
+
+      await FirebaseService.newCategory(docName, data);
+    } catch (e) {
+      throw Exception(e);
+    }
+  }
+
+  static Future<String>? validateNewCategory(String category) async {
+    return FirebaseService.getCollectionsSnapshot('Categories')
+        .where(category, isEqualTo: category)
+        .get()
+        .toString();
   }
 }
